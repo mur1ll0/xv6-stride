463d4ef2ffc0fa61aed9f83a3bf46108df291582
Murillo Andre Maleski <murillomaleski@gmail.com>
2018-11-08 21:25:01 -0200

    Add files via upload

---
+++ teste.c
@@ -0,0 +1,39 @@
+#include "types.h"
+#include "user.h"
+
+int teste(int pid){
+  int i, j;
+
+  for(i=0; i<10;i++){
+    for(j=0; j<200;j++){
+      printf(1, "Processo executando: %d\n", getpid());
+    }
+  }
+
+  printf(1, "TERMINOU %d\n", getpid());
+
+  exit();
+}
+
+int main(){
+  int numProc = 3;  //Definir numero de processos a serem alocados
+  int i, pid;
+
+  for(i=0;i<numProc;i++){
+    pid = fork();
+
+    if(pid < 4){
+      settickets(10);
+      printf(1, "Processo %d tem 10 tickets\n", getpid());
+    }
+    else if(pid < 8){
+      settickets(200);
+      printf(1, "Processo %d tem 200 tickets\n", getpid());
+    }
+    else{
+      settickets(1000);
+      printf(1, "Processo %d tem 1000 tickets\n", getpid());
+    }
+    teste(pid);
+  }
+}
